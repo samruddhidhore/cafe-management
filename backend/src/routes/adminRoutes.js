85d2c227832842b1cd7cc2429519217ba264b0ca
Shravani Bule <shravanibule09@gmail.com>
2026-05-28 00:09:41 +0530

    Remove node_modules from tracking

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("./admin.controller");
+const controller = require("../controllers/adminController");
 
-// MENU
+/* MENU */
 router.post("/menu", controller.addMenu);
+router.get("/menu", controller.getMenu);
 router.put("/menu/:id", controller.editMenu);
 router.delete("/menu/:id", controller.deleteMenu);
-router.get("/menu", controller.getMenu);
+router.patch("/menu/toggle/:id", controller.toggleAvailability);
 
-// ORDERS
+/* ORDERS */
 router.post("/orders", controller.addOrder);
 router.get("/orders", controller.getOrders);
+router.get("/orders/status/:status", controller.getOrdersByStatus);
 router.put("/orders/:id", controller.updateOrderStatus);
+
+/* ANALYTICS */
+router.get("/analytics/top-selling", controller.getTopSelling);
 
 module.exports = router;
