0ff816f9282f5b93c256a5e1b113aa0c3faec27b
AnushkaBhuskat <anu.bhuskat@gmail.com>
2026-05-27 16:39:41 +0530

    Added backend structure and billing APIs for cafe management system

---
+++ backend/src/routes/billRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const {
+  generateBill,
+  getAllBills
+} = require('../controllers/billController');
+
+const router = express.Router();
+
+router.post('/generate', generateBill);
+
+router.get('/all', getAllBills);
+
+module.exports = router;
