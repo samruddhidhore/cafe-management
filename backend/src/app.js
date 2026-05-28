d9416a3a6797362874b7bd4a3bee1f60edfe3c3a
gauripakhare <gauripakhare2005@gmail.com>
2026-05-28 14:33:54 +0530

    backend connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const adminRoutes = require("./routes/adminRoutes");
+const orderRoutes = require("./routes/orderRoutes")
 const billRoutes = require('./routes/billRoutes');
 
 const app = express();
@@ -8,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/admin", adminRoutes);
+app.use('/api/orders', orderRoutes);
 app.use('/api/bill', billRoutes);
 
 module.exports = app;
