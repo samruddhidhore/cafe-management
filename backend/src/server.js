76b42da029d96b9dc17b40ab572330c09e7de46c
AnushkaBhuskat <anu.bhuskat@gmail.com>
2026-05-28 13:04:31 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const orderRoutes = require("./routes/orderRoutes");
+const billRoutes = require("./routes/billRoutes");
 
 app.use("/api/orders", orderRoutes);
+app.use('/api/bill', billRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
