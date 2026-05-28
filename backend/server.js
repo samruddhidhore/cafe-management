140bfc410eaaa26933365a7b4c686c9a6e7fe86d
AnushkaBhuskat <anu.bhuskat@gmail.com>
2026-05-28 14:50:39 +0530

    changes done in backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 const PORT = 5000;
 
-<<<<<<< HEAD:backend/server.js
 app.use(cors());
 app.use(express.json());
 
@@ -14,8 +13,4 @@ app.use('/api/bill', billRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
-=======
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
->>>>>>> menu-api:backend/src/server.js
 });
